ee09039984472524ef4410add547c54e1d814c16
Josef <josefch@gmail.com>
2023-07-02 20:20:13 -0500

    find files match by md5 sums

--- file_YMD_match/cmd/main.go
+++ file_YMD_match/cmd/main.go
@@ -2,18 +2,47 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
 	"os"
+	"path/filepath"
+	"strings"
 	"time"
 
 	a "github.com/chlachula/go2/file_YMD_match"
 )
 
+func f1(sourceDir, targetDir string) {
+	err := filepath.Walk(sourceDir, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
+			return err
+		}
+		//fmt.Printf("visited file or dir: %q\n", path)
+		if f, err := os.Stat(path); err == nil {
+			if !f.IsDir() {
+				a.ExistsYMD(strings.ReplaceAll(path, "\\", "/"), targetDir)
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		fmt.Printf("error walking the path %q: %v\n", sourceDir, err)
+		return
+	}
+}
+func f2(toProcessFile, processed string) {
+	tArr := a.SumsToArr(toProcessFile)
+	pArr := a.SumsToArr(processed)
+	fmt.Printf("Zpracovat: %d, zpracovano: %d \n", len(tArr), len(pArr))
+	a.FindMatches(tArr, pArr)
+}
+
 func help() {
 	helptext := `
 Usage:
-go2 sourcePath targetDir 
+go2 sourceDir targetDir 
 Example:
-go2  /e/Arc-Pics/2003/2003_06_22Ne/BoziTelo22 020.jpg /e/Arc-Pics/Zpracovat
+go2  /e/Arc-Pics/2003 /e/Arc-Pics/Zpracovat
 	`
 	fmt.Println(helptext)
 }
@@ -25,5 +54,7 @@ func main() {
 		help()
 		os.Exit(1)
 	}
-	fmt.Printf("%t\n", a.ExistsYMD(os.Args[1], os.Args[2]))
+	//fmt.Printf("%t\n", a.ExistsYMD(os.Args[1], os.Args[2]))
+	//f1(os.Args[1], os.Args[2])
+	f2(os.Args[1], os.Args[2])
 }
--- file_YMD_match/file_YMD_match.go
+++ file_YMD_match/file_YMD_match.go
@@ -1,12 +1,20 @@
 package file_YMD_match
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"regexp"
-)
+	"strings"
+	/* "github.com/udhos/equalfile" */)
 
 var re1 = regexp.MustCompile(`(?m)(?P<src>.+)\/(?P<yyyy>\d{4})_(?P<mm>\d\d)_(?P<dd>\d\d)(?P<x>.*)\/(?P<filename>.+)`)
+
+type RecordType = struct {
+	md5   string
+	path  string
+	Path2 string
+}
 
 func getParams(compRegEx *regexp.Regexp, str string) (paramsMap map[string]string) {
 	paramsMap = make(map[string]string)
@@ -21,6 +29,61 @@ func getParams(compRegEx *regexp.Regexp, str string) (paramsMap map[string]strin
 	return paramsMap
 }
 
+func findMatch(t RecordType, processed []RecordType) (bool, RecordType) {
+	for _, p := range processed {
+		if p.md5 == t.md5 {
+			return true, p
+		}
+	}
+	return false, t
+}
+
+// to find file matches
+// find . -type f -exec md5sum {} \; >> toProcess.md5sums #or processes.md5sums
+func FindMatches(toProcess, processed []RecordType) {
+	f := 0
+	for it, t := range toProcess {
+		if found, r := findMatch(t, processed); found {
+			toProcess[it].Path2 = r.path
+			f += 1
+		}
+	}
+	var a, b int
+	for _, t := range toProcess {
+		if t.Path2 != "" {
+			a += 1
+		} else {
+			fmt.Printf("%v\n", t)
+			b += 1
+		}
+	}
+	fmt.Printf("found= %d, a=%d + b=%d = %d \n", f, a, b, len(toProcess))
+
+}
+func SumsToArr(filePath string) []RecordType {
+	fileLines := make([]RecordType, 0)
+	readFile, err := os.Open(filePath)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fileScanner := bufio.NewScanner(readFile)
+	defer readFile.Close()
+	fileScanner.Split(bufio.ScanLines)
+
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		s := strings.Split(line, " ")
+		if len(s) == 2 {
+			var rec RecordType
+			rec.md5 = s[0]
+			rec.path = s[1]
+			fileLines = append(fileLines, rec)
+		}
+	}
+	return fileLines
+}
+
 // returns true if sourceDir/yyyy_mm_dd/filename exists
 // at      targetDir/yyyy/mm/yyyy_mm_dd/filename
 func ExistsYMD(sourcePathname, targetDir string) bool {
@@ -32,12 +95,29 @@ func ExistsYMD(sourcePathname, targetDir string) bool {
 		x := p["x"]
 		filename := p["filename"]
 		target := fmt.Sprintf("%s/%s/%s/%s_%s_%s%s/%s", targetDir, yyyy, mm, yyyy, mm, dd, x, filename)
-		fmt.Println("DEBUG target = ", target)
 		if _, err := os.Stat(target); err == nil {
+			/*
+				equal, err := equalfile.CompareFile(sourcePathname, target)
+				if err != nil {
+					fmt.Printf("equal: error: %v\n", err)
+					os.Exit(3)
+				}
+
+				if equal {
+					fmt.Println("equal: files match")
+					os.Exit(0)
+				}
+
+				fmt.Println("equal: files differ")
+			*/
+			//fmt.Println("DEL ", target)
 			return true
 		} else {
+			fmt.Println("echo NO-MATCH ", sourcePathname)
 			return false
 		}
+	} else {
+		fmt.Println("echo NO-PATTERN ", sourcePathname)
 	}
 	return false
 }
