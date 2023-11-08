b81e67e2ee1b28c175df33cb51d8a1e579134d7d
Josef <josefch@gmail.com>
2023-11-08 17:25:43 -0600

    tour: switch true

--- tour/tour.go
+++ tour/tour.go
@@ -127,4 +127,14 @@ func Switches() {
 	}
 	fmt.Printf("Time %s has %s minute\n", now.String(), d2s)
 
+	//switch true
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good morning!")
+	case t.Hour() < 17:
+		fmt.Println("Good afternoon.")
+	default:
+		fmt.Println("Good evening.")
+	}
 }
