65caae9a5e1d405b8291eb958903ed017244397c
Josef <josefch@gmail.com>
2023-11-28 00:11:26 -0600

    tour: Pointers

--- tour/tour.go
+++ tour/tour.go
@@ -104,6 +104,18 @@ func Loops() {
 	}
 	fmt.Println()
 }
+func Pointers(){
+	i, j := 42, 2701
+
+	p := &i         // point to i
+	fmt.Println(*p) // read i through the pointer
+	*p = 21         // set i through the pointer
+	fmt.Println(i)  // see the new value of i
+
+	p = &j         // point to j
+	*p = *p / 37   // divide j through the pointer
+	fmt.Println(j) // see the new value of j
+}
 func Switches() {
 	switch os := runtime.GOOS; os {
 	case "windows":
