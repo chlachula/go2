6298a89ad13d9640fe42ebf40068b5f6f7f06a26
Josef <josefch@gmail.com>
2023-11-29 21:27:17 -0600

    diffsecs: test update part1

--- diffsecs/diffsecs_test.go
+++ diffsecs/diffsecs_test.go
@@ -6,12 +6,22 @@ import (
 	"time"
 )
 
+func diffDatesSecs(y1 int, m1 time.Month, d1, h1, mi1, s1 int, y2 int, m2 time.Month, d2, h2, mi2, s2 int) (float64, error) {
+	date1 := time.Date(y1, m1, d1, h1, mi1, s1, 0, time.UTC)
+	date2 := time.Date(y2, m2, d2, h2, mi2, s2, 0, time.UTC)
+	sec, err := DatesDiffInSeconds(date1, date2)
+	return sec, err
+}
+
 func TestInc1sec(t *testing.T) {
 	expectedSec := 86401.0
 
-	d1 := time.Date(1972, time.January, 1, 0, 0, 0, 0, time.UTC)
-	d2 := time.Date(1972, time.January, 2, 0, 0, 0, 0, time.UTC)
-	sec, err := DatesDiffInSeconds(d1, d2)
+	/*
+		d1 := time.Date(1972, time.January, 1, 0, 0, 0, 0, time.UTC)
+		d2 := time.Date(1972, time.January, 2, 0, 0, 0, 0, time.UTC)
+		sec, err := DatesDiffInSeconds(d1, d2)
+	*/
+	sec, err := diffDatesSecs(1972, time.January, 1, 0, 0, 0, 1972, time.January, 2, 0, 0, 0)
 	if err != nil {
 		t.Errorf("error:unexpected seconds %f instead of %f between %s..%s", sec, 86400.0, d1.Format("2006.01.02 15h"), d2.Format("2006.01.02 15h"))
 	}
