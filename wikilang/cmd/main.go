2a6029d8f34a5d2f9722e83f0dd7fd11ff3774a4
Josef <josefch@gmail.com>
2023-12-14 14:09:34 -0600

    wikilang: help extended

--- wikilang/cmd/main.go
+++ wikilang/cmd/main.go
@@ -3,11 +3,21 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
 	a "github.com/chlachula/go2/wikilang"
 )
 
+func quotesRemove(s string) string {
+	if len(s) > 0 && s[0] == '"' {
+		s = s[1:]
+	}
+	if len(s) > 0 && s[len(s)-1] == '"' {
+		s = s[:len(s)-1]
+	}
+	return s
+}
 func help(msg string) {
 	if msg != "" {
 		fmt.Println(msg)
@@ -16,7 +26,8 @@ func help(msg string) {
 	Usage:
 	go2 url lang
 	Example:
-	go2 https://en.wikipedia.org/wiki/Cassiopeia_(constellation) cs
+	go2 "https://en.wikipedia.org/wiki/Cassiopeia_(constellation)" cs
+	go2 https://en.wikipedia.org/wiki/Tau_function cs
 	`
 	fmt.Println(helptext)
 }
@@ -24,10 +35,21 @@ func main() {
 	defer func(start time.Time) {
 		fmt.Printf("Elapsed time %s\n", time.Since(start))
 	}(time.Now())
+	if len(os.Args) < 2 {
+		help("Not enough arguments")
+		os.Exit(1)
+	}
+	if strings.HasPrefix(os.Args[1], "-h") {
+		help("")
+		os.Exit(0)
+	}
+	if len(os.Args) < 3 {
+		help("Not enough arguments")
+		os.Exit(1)
+	}
 
-	urlString := "https://en.wikipedia.org/wiki/Cassiopeia_(constellation)"
-	lang := "cs"
-	//help("")
+	urlString := quotesRemove(os.Args[1])
+	lang := os.Args[2]
 	page, err := a.DownloadPage(urlString)
 	if err != nil {
 		fmt.Println(err.Error())
@@ -38,6 +60,12 @@ func main() {
 		fmt.Println(err1.Error())
 		os.Exit(1)
 	}
-	fmt.Println("For language", lang, "found for ", urlString, "\nlink: ", langLink, "\nname: ", name)
+	if langLink == "" {
+		langLink = "NOT FOUND"
+	}
+	if name == "" {
+		name = "N/A"
+	}
+	fmt.Println("For language", lang, "found \nfor : ", urlString, "\nlink: ", langLink, "\nname: ", name)
 
 }
