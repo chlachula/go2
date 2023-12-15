178c524d77e9a46a571b0e12300e91742e9315ec
Josef <josefch@gmail.com>
2023-12-15 09:56:33 -0600

    wikilang: main func wikiLanguageVersion

--- wikilang/cmd/main.go
+++ wikilang/cmd/main.go
@@ -18,38 +18,10 @@ func quotesRemove(s string) string {
 	}
 	return s
 }
-func help(msg string) {
-	if msg != "" {
-		fmt.Println(msg)
-	}
-	helptext := `Displays link to the alternative language version
-	Usage:
-	go2 url lang
-	Example:
-	go2 "https://en.wikipedia.org/wiki/Cassiopeia_(constellation)" cs
-	go2 https://en.wikipedia.org/wiki/Tau_function cs
-	`
-	fmt.Println(helptext)
-}
-func main() {
-	defer func(start time.Time) {
-		fmt.Printf("Elapsed time %s\n", time.Since(start))
-	}(time.Now())
-	if len(os.Args) < 2 {
-		help("Not enough arguments")
-		os.Exit(1)
-	}
-	if strings.HasPrefix(os.Args[1], "-h") {
-		help("")
-		os.Exit(0)
-	}
-	if len(os.Args) < 3 {
-		help("Not enough arguments")
-		os.Exit(1)
-	}
+func wikiLanguageVersion(urlWiki, langVersion string) {
+	urlString := quotesRemove(urlWiki)
+	lang := strings.ToLower(langVersion)
 
-	urlString := quotesRemove(os.Args[1])
-	lang := os.Args[2]
 	page, err := a.DownloadPage(urlString)
 	if err != nil {
 		fmt.Println(err.Error())
@@ -67,5 +39,39 @@ func main() {
 		name = "N/A"
 	}
 	fmt.Println("For language", lang, "found \nfor : ", urlString, "\nlink: ", langLink, "\nname: ", name)
+
+}
+func help(msg string) {
+	if msg != "" {
+		fmt.Println(msg)
+	}
+	helptext := `Displays link to the alternative language version
+	Usage:
+	go2 url lang
+	Example:
+	go2 "https://en.wikipedia.org/wiki/Cassiopeia_(constellation)" cs
+	go2 https://en.wikipedia.org/wiki/Tau_function cs
+	`
+	fmt.Println(helptext)
+}
+func main() {
+	defer func(start time.Time) {
+		fmt.Printf("Elapsed time %s\n", time.Since(start))
+	}(time.Now())
+
+	if len(os.Args) < 2 {
+		help("Not enough arguments")
+		os.Exit(1)
+	}
+	if strings.HasPrefix(os.Args[1], "-h") {
+		help("")
+		os.Exit(0)
+	}
+	if len(os.Args) < 3 {
+		help("Not enough arguments")
+		os.Exit(1)
+	}
+
+	wikiLanguageVersion(os.Args[1], os.Args[2])
 
 }
