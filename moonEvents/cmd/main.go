2a2898996f1f521a06ba3102ca445444cb47b0ca
Josef <josefch@gmail.com>
2023-12-18 16:05:57 -0600

    moonEvents: main init functionality

--- moonEvents/cmd/main.go
+++ moonEvents/cmd/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"log"
+	"net/http"
+	"os"
+	"sort"
 
 	a "github.com/chlachula/go2/moonEvents"
 )
@@ -12,14 +15,32 @@ func help(msg string) {
 		fmt.Println(msg)
 	}
 	helptext := `Events calendar with moon phases background
+	-h this help
+	-p [port] preview to port 8080 
+	-c generate google calendar  
 	`
 	fmt.Println(helptext)
 }
 func main() {
-	defer func(start time.Time) {
-		fmt.Printf("Elapsed time %s\n", time.Since(start))
-	}(time.Now())
+	a.LoadData()
+	sort.Sort(a.ByDate(a.EventsData.Event))
 
-	help("")
-	a.Hello()
+	argsWithoutProg := os.Args[1:]
+	if len(argsWithoutProg) < 1 {
+		help("")
+	}
+	if argsWithoutProg[0] == "-c" {
+		filename := fmt.Sprintf("GoogleCal-events-%d.csv", a.EventsData.YYYY)
+		a.GenerateGoogleCalendarListCSV(a.EventsData, filename)
+	} else if argsWithoutProg[0] == "-p" {
+		http.HandleFunc("/", a.EventHandler)
+		http.HandleFunc("/api", a.ApiHandler)
+		http.Handle("/files", http.FileServer(http.Dir("public/")))
+		colonPort := ":8080"
+		if len(argsWithoutProg) > 1 {
+			colonPort = ":" + argsWithoutProg[1]
+		}
+		print("...listening at " + colonPort)
+		log.Fatal(http.ListenAndServe(colonPort, nil))
+	}
 }
