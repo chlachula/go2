ae774f7dc3821c0f24e1bbaabdc0765c8c63fd7c
Josef <josefch@gmail.com>
2024-01-18 09:47:14 -0600

    dotdir: /filesN/,

--- dotdir/cmd/main.go
+++ dotdir/cmd/main.go
@@ -2,14 +2,43 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 	"time"
 
 	a "github.com/chlachula/go2/dotdir"
 )
 
+func help(msg string) {
+	if msg != "" {
+		fmt.Println(msg)
+	}
+	text := `Program dotdir shows absolute path of the current directory.
+	Current directory is represented by single '.' .
+	Program shows also content of the directory on stdout and on webpage /dot .text
+	It can be usefull to explore unknown environment like containers.
+	Usage:
+	no arguments .. display info on stdout and starts webserver on port 8080
+	-h this help
+	-p PORT_NUMBER .. defines webserver port number`
+	fmt.Println(text)
+}
+
 func main() {
 	defer func(start time.Time) {
 		fmt.Printf("Elapsed time %s\n", time.Since(start))
 	}(time.Now())
+	colonPort := ":8080"
+	if len(os.Args) > 1 {
+		if strings.HasPrefix(os.Args[1], "-h") {
+			help("")
+			return
+		}
+		if strings.HasPrefix(os.Args[1], "-p") && len(os.Args) > 2 {
+			colonPort = ":" + os.Args[2]
+		}
+	}
+
 	fmt.Println(a.DirAbsInfo("."))
+	a.Web(colonPort)
 }
--- dotdir/dotdir.go
+++ dotdir/dotdir.go
@@ -2,9 +2,16 @@ package dotdir
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 	"path/filepath"
 )
+
+func DotWebHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<html><head><title>dot dir</title></head><body><h1>dot dir</h1>\n<pre>")
+	absDot := DirAbsInfo(".")
+	fmt.Fprintf(w, "%s\n</pre>\n", absDot)
+}
 
 func DirAbsInfo(dir string) string {
 	abs, err := filepath.Abs(dir)
@@ -38,5 +45,68 @@ func DirAbsInfo(dir string) string {
 	s += fmt.Sprintf(": %d directories, %d files.", countDirs, countFiles)
 	s += "\nFiles: " + files
 	s += "\nDirectories: " + dirs
-	return s
+	return s + "\n"
+}
+
+func Web(colonPort string) {
+
+	fs := http.FileServer(http.Dir("files"))
+	prefixHandler := http.StripPrefix("/files1/", fs)
+	http.Handle("/files1/", prefixHandler)
+	fmt.Print("OK: handle to /files1/ \n")
+
+	relFiles := "./files"
+	sF2s := "/files2/"
+	if stat, err := os.Stat(relFiles); err == nil && stat.IsDir() {
+		http.Handle(sF2s, http.StripPrefix(sF2s, http.FileServer(http.Dir(relFiles))))
+		fmt.Print("OK: handle to /files2/ \n")
+	} else {
+		fmt.Printf("error:  %s\n\n", err.Error())
+	}
+
+	if absFiles, err := filepath.Abs(relFiles); err == nil {
+		sF3s := "/files3/"
+		fmt.Printf("filepath.Abs(\"%s\"): %s\n", relFiles, absFiles)
+		http.Handle(sF3s, http.StripPrefix(sF3s, http.FileServer(http.Dir(absFiles))))
+		fmt.Print("OK: handle to /files3/ \n")
+	} else {
+		fmt.Println(err.Error())
+	}
+
+	http.HandleFunc("/", RootHandler)
+	http.HandleFunc("/dot", DotHandler)
+
+	print("...listening at " + colonPort)
+	if err := http.ListenAndServe(colonPort, nil); err != nil {
+		fmt.Println("error ", err.Error())
+	}
+}
+
+func home(homelink bool) string {
+	link := "home"
+	part := `<html><head><title>dot dir</title></head><body style="text-align:center">
+	%s
+	<hr size="1" noshadow="noshadow" />
+	`
+	if homelink {
+		link = `<a href="/">home</a>`
+	}
+	return fmt.Sprintf(part, link)
+}
+func RootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, home(false))
+	part := `<h1>Hello <a href="/dot">dot</a>!</h1>
+	Handles to the subdirectory files<br/>
+	<a href="/files1/">/files1/</a><br/>
+	<a href="/files2/">/files2/</a><br/>
+	<a href="/files3/">/files3/</a><br/>
+	</body></html>`
+	fmt.Fprint(w, part)
+}
+func DotHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, home(true))
+	part1 := "<h1>Dot directory - current directory information</h1>\n<pre>\n"
+	fmt.Fprint(w, part1)
+	fmt.Fprint(w, DirAbsInfo("."))
+	fmt.Fprint(w, "</pre></body></html>")
 }
