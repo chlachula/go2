779eafd70898efa8adae48dc28d39dfeb7fc8af3
Josef <josefch@gmail.com>
2024-02-06 21:39:37 -0600

    dirreport: displayDirectoryContents

--- dirreport/dirreport.go
+++ dirreport/dirreport.go
@@ -3,6 +3,8 @@ package dirreport
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
 	"text/template"
 )
 
@@ -41,6 +43,49 @@ func HandleShowDir(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "<h1>error %s</h1>", err.Error())
 		}
 	}
-
+	if str, err := displayDirectoryContents(Dir); err == nil {
+		fmt.Fprint(w, "\n<pre>\n"+str+"\n</pre>\n")
+	} else {
+		fmt.Fprintf(w, "<h2>%s</h2>", err.Error())
+	}
 	fmt.Fprint(w, htmlEnd)
+}
+func displayDirectoryContents(dirPath string) (string, error) {
+	s := ""
+	// Open the directory
+	dir, err := os.Open(dirPath)
+	if err != nil {
+		return "", err
+	}
+	defer dir.Close()
+
+	// Read the contents of the directory
+	fileInfos, err := dir.Readdir(-1)
+	if err != nil {
+		return "", err
+	}
+
+	// Loop through each file/directory
+	for _, fileInfo := range fileInfos {
+		fullPath := filepath.Join(dirPath, fileInfo.Name())
+
+		// Print file/directory information
+		s += fmt.Sprintln("Name:", fileInfo.Name())
+		s += fmt.Sprintln("Size:", fileInfo.Size(), "bytes")
+		s += fmt.Sprintln("Modified Time:", fileInfo.ModTime())
+		s += fmt.Sprintln("Mode:", fileInfo.Mode())
+
+		// Check if the file/directory is a directory
+		if fileInfo.IsDir() {
+			// If it is a directory, recursively call this function
+			if s2, err := displayDirectoryContents(fullPath); err != nil {
+				return s, err
+			} else {
+				s += s2
+			}
+		}
+		s += "\n"
+		//fmt.Println()
+	}
+	return s, nil
 }
