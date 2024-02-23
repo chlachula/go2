7921b5eea9aa816202e8ff7993c3179d1a6ce98f
Josef <josefch@gmail.com>
2024-02-23 17:49:21 -0600

    dirreport: test for max int64

--- dirreport/dirreport_test.go
+++ dirreport/dirreport_test.go
@@ -33,4 +33,11 @@ func TestNum10p3str(t *testing.T) {
 	if want != got {
 		t.Errorf("want %s, got %s for number %d", want, got, n)
 	}
+
+	n = int64(^uint64(0) >> 1) // max int64
+	want = "9.2E"              // Exa
+	got = num10p3str(n)
+	if want != got {
+		t.Errorf("want %s, got %s for number %d", want, got, n)
+	}
 }
