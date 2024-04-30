742f6544f96f25c52c45d3b60101c307aaa2c3e2
Josef <josefch@gmail.com>
2024-04-30 17:04:31 -0500

    diffsecs: help typo

--- diffsecs/cmd/main.go
+++ diffsecs/cmd/main.go
@@ -26,7 +26,7 @@ Usage:
 go2 -h
 go2 -l #list of the leap seconds
 go2 -d yyyy.mm.dd1 yyyy.mm.dd2
-go2 -d yyyy.mm.dd1-HH"MM"SS.sssssssss yyyy.mm.dd2-HH"MM"SS.sssssssss
+go2 -d yyyy.mm.dd1-HH:MM:SS.sssssssss yyyy.mm.dd2-HH:MM:SS.sssssssss
 Examples:
 go2 -d 2016.12.31 2017.1.1
 go2 -d 2016.12.31-23:59:00.0 2017.1.1
