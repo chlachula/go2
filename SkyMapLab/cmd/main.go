b0c1d87a241b9ac05af9eaecfbb9d1f307691c1c
Josef <josefch@gmail.com>
2024-05-12 11:55:04 -0500

    SkyMapLab: radius and lat arguments

--- SkyMapLab/cmd/main.go
+++ SkyMapLab/cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 
@@ -19,6 +20,10 @@ func underscoredText(i int) string {
 		return ""
 	}
 }
+func toFloat64(str string) float64 {
+	f, _ := strconv.ParseFloat(str, 64)
+	return f
+}
 func help(msg string) {
 	if msg != "" {
 		fmt.Printf("%s \n\n", msg)
@@ -26,6 +31,8 @@ func help(msg string) {
 	helptext := `SkyMap Lab to create mostly visual objects sky maps in SVG
  Usage:
  -h this help
+ -l latitude
+ -r radius
  -p [port] preview to port 8080 
  Example:
 
@@ -36,6 +43,8 @@ func main() {
 	defer func(start time.Time) {
 		fmt.Printf("Elapsed time %s\n", time.Since(start))
 	}(time.Now())
+	radius := 150.0
+	lat := 44.0
 
 	colonPort := ":8080"
 	if len(os.Args) < 2 {
@@ -46,12 +55,23 @@ func main() {
 			case "-h":
 				help("")
 				os.Exit(0)
+			case "-l":
+				if len(os.Args) > i+1 {
+					lat = toFloat64(os.Args[i+1])
+					i += 1
+				}
+			case "-r":
+				if len(os.Args) > i+1 {
+					radius = toFloat64(os.Args[i+1])
+					i += 1
+				}
 			case "-p":
 				if len(os.Args) > i+1 {
 					colonPort = ":" + os.Args[i+1]
 				}
 				a.LoadStars("data/SkyMap-stars.json")
 				a.LoadConstellations("data/SkyMap-constellations.json")
+				a.SetMapStyle(radius, lat, a.MapColorsRed)
 				http.HandleFunc("/", a.HandlerHome)
 				http.HandleFunc("/img/svg-skymap-color", a.HandlerImageSkymapColor)
 				http.HandleFunc("/img/svg-skymap-bw", a.HandlerImageSkymapBW)
