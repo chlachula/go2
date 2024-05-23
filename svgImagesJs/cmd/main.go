237d9369b2bf6e11b1631e7730333b258c8cf8be
Josef <josefch@gmail.com>
2024-05-23 09:13:13 -0500

    svgImagesJs: web

--- svgImagesJs/cmd/main.go
+++ svgImagesJs/cmd/main.go
@@ -1,8 +1,51 @@
 package main
 
-import "fmt"
-import a "github.com/chlachula/go2/svgImagesJs"
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
 
-func main(){
-  fmt.Println(a.Atext)
+	a "github.com/chlachula/go2/svgImagesJs"
+)
+
+func help(msg string) {
+	if msg != "" {
+		fmt.Printf("%s \n\n", msg)
+	}
+	helptext := `Web: SVG on HTML pages with javascript
+ Usage:
+ -h this help
+ -p [port] preview to port 8080 
+ Example:
+ -p
+`
+	fmt.Println(helptext)
+}
+func main() {
+	defer func(start time.Time) {
+		fmt.Printf("Elapsed time %s\n", time.Since(start))
+	}(time.Now())
+
+	colonPort := ":8080"
+	if len(os.Args) < 2 {
+		help("Not enough arguments")
+	} else {
+		for i := 1; i < len(os.Args); i++ {
+			switch arg := os.Args[i]; arg {
+			case "-h":
+				help("")
+				os.Exit(0)
+			case "-p":
+				http.HandleFunc("/", a.HandlerHome)
+				http.HandleFunc("/htmlSvgImagesJs", a.HandlerHtmlJsSvgPages)
+				http.HandleFunc("/eyepieceLabels", a.HandlerEyepieceLabels)
+				what := "HTML+javascript SVG "
+				print("Serving " + what + "at " + colonPort + ". CTRL+C to stop.")
+				http.ListenAndServe(colonPort, nil)
+			default:
+				help("Unexpected argument " + arg)
+			}
+		}
+	}
 }
--- svgImagesJs/svgImagesJs.go
+++ svgImagesJs/svgImagesJs.go
@@ -1,3 +1,43 @@
 package svgImagesJs
 
+import (
+	"fmt"
+	"net/http"
+)
+
 var Atext string = "text sample"
+
+func HandlerHome(w http.ResponseWriter, r *http.Request) {
+	//writeHtmlHeadAndMenu(w, "/", "Home")
+	fmt.Fprint(w, `<html>
+ <head>
+ <meta http-equiv="refresh" content="0; url=/htmlSvgImagesJs">
+ 	  <title>redirect to EyepieceLabels</title>
+ </head>
+ <body>
+  <h1>Click to: <a href="/htmlSvgImagesJs">htmlSvgImagesJs</a></h1>
+ </body>
+</html>
+	`)
+}
+func HandlerHtmlJsSvgPages(w http.ResponseWriter, r *http.Request) {
+	//writeHtmlHeadAndMenu(w, "/svg-roundlogo-color", "Color")
+
+	fmt.Fprint(w, "<html><head><title>HTML javascript SVG pages</title></head>\n")
+	fmt.Fprint(w, "<body style=\"text-align: center;\">\n")
+	fmt.Fprint(w, "<h1>HTML javascript SVG pages</h1>\n")
+	fmt.Fprint(w, "<h1><a href=\"/eyepieceLabels\">Eyepiece Labels</a></h1>\n")
+
+	fmt.Fprint(w, "</body></html>\n")
+
+}
+func HandlerEyepieceLabels(w http.ResponseWriter, r *http.Request) {
+	//writeHtmlHeadAndMenu(w, "/svg-roundlogo-color", "Color")
+
+	fmt.Fprint(w, "<html><head><title>Eyepiece Labels</title></head>\n")
+	fmt.Fprint(w, "<body style=\"text-align: center;\">\n")
+	fmt.Fprint(w, "<h1>Eyepiece Labels</h1>\n")
+
+	fmt.Fprint(w, "</body></html>\n")
+
+}
