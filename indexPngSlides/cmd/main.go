c81308e423f33d16955a0b88b2d01fc4c3cc69b7
Josef <josefch@gmail.com>
2024-07-29 14:58:58 -0500

    indexPngSlides: PNG files

--- indexPngSlides/cmd/main.go
+++ indexPngSlides/cmd/main.go
@@ -1,13 +1,16 @@
 package main
+
 import (
-"fmt"
-"time"
-a "github.com/chlachula/go2/indexPngSlides"
+	"fmt"
+	"time"
+
+	a "github.com/chlachula/go2/indexPngSlides"
 )
+
 func main() {
 	defer func(start time.Time) {
 		fmt.Printf("Elapsed time %s\n", time.Since(start))
 	}(time.Now())
-        
-        fmt.Println(a.Hello)
+
+	a.FindPngFiles("testdir")
 }
--- indexPngSlides/indexPngSlides.go
+++ indexPngSlides/indexPngSlides.go
@@ -1,3 +1,31 @@
 package indexPngSlides
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
 var Hello string = "Hello World"
+
+func FindPngFiles(dir string) {
+	var PNGfiles []string = make([]string, 0)
+	d, err := os.Open(dir)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	files, err := d.Readdir(0)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, f := range files {
+		fmt.Printf("name=%s, isDir=%t   %+v\n", f.Name(), f.IsDir(), f)
+		if strings.HasSuffix(f.Name(), ".png") {
+			PNGfiles = append(PNGfiles, f.Name())
+		}
+	}
+	fmt.Printf("PNG files  %+v\n", PNGfiles)
+}
