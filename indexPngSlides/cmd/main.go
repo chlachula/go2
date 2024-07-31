3e548148b58d60e9c12087ba80e85d88569f6503
Josef <josefch@gmail.com>
2024-07-30 21:41:07 -0500

    indexPngSlides: cmd dirWithPNGs argument

--- indexPngSlides/cmd/main.go
+++ indexPngSlides/cmd/main.go
@@ -2,15 +2,33 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 
 	a "github.com/chlachula/go2/indexPngSlides"
 )
 
+func help(msg string) {
+	helpText := `
+Program indexPngSlides put not tall PNG slide images to the index.html page
+Usage:
+ go2 directory //containing png files
+Example:
+ go2 testdir
+`
+	if msg != "" {
+		fmt.Println(msg)
+	}
+	fmt.Println(helpText)
+}
 func main() {
 	defer func(start time.Time) {
 		fmt.Printf("Elapsed time %s\n", time.Since(start))
 	}(time.Now())
-
-	a.FindPngFiles("testdir")
+	if len(os.Args) < 2 {
+		help("No directory argument")
+		os.Exit(1)
+	}
+	dirWithPNGs := os.Args[1]
+	a.CreateIndexFile(dirWithPNGs)
 }
